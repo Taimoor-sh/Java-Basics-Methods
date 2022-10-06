e6b0be3df09a2f119c76c0cd2e1ce378c7c7bd71
Taimoor-sh <106023544+Taimoor-sh@users.noreply.github.com>
2022-10-06 17:36:01 +0500

    Add files via upload
    Pre defined methods practice in java using user defined methods

--- program.java
+++ program.java
@@ -1,29 +1,21 @@
-import java.util.Scanner;
- /**
-  * program
-  */
- public class program {
+/**
+ * program
+ */
+public class program {
+
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter 1st Value :"); 
-    int i = sc.nextInt();
-    System.out.println("Enter 2nd Value :");
-    int j = sc.nextInt(); 
-    program obj = new program();
-  add(i, j);
-  obj.sub(i, j);
-  mul(i, j);
+    MaxNum();
+    SquareRoot();
+    Length();
   }
- static void add(int a , int b) {
-     long c = a + b ;
-    System.out.println("Sum of both values is : " + c );
+  public static void MaxNum() {
+    System.out.println("Maximum Number is : " + Math.max(5,6));
   }
-   void sub(int a , int b) {
-    long c = a - b ;
-   System.out.println("Subtraction of both values is : " + c );
- }
- static void mul(int a , int b) {
-  long c = a * b ;
- System.out.println("Multiplecation of both values is : " + c );
+  public static void SquareRoot() {
+    System.out.println("Square Root of 16 is : " + Math.sqrt(16));
+  }
+  public static void Length() {
+    String name = "Taimoor";
+    System.out.println("Character length of name Taimoor is : " + name.length() );
+  }
 }
- }
